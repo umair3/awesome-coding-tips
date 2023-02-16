631a2c0d0953b4f1ff7a3837ae97ca1f5ed17161
Rizwan Ahmed <rizwanahmedg2020@gmail.com>
2023-02-16 05:57:01 +0500

    Drivable Class Method

--- solid_principles/interface_segregation.py
+++ solid_principles/interface_segregation.py
@@ -4,11 +4,70 @@ In the below example we are segregating Driveable interface into
 multiple interaces including DrivableOnRoad, Flyable, Rideable.
 """
 from abc import ABC, abstractmethod
+import warnings
 
 
-class Driveable(ABC):
-    pass
+class Driveable():
 
+    STEERING_MAX_ROTATION = {
+        'ANTICLOCKWISE' : -720,
+        'CLOCKWISE' : 720
+    }
+
+    class Value_Error(Exception):
+        Exception(ValueError("Value Erro"))
+
+    # worst case
+    @abstractmethod
+    def accelerate(*args):
+        print(args)
+
+    # best case
+    @abstractmethod
+    def accelerate(self, check):
+        return 
+
+    @abstractmethod
+    def brake(self, check):
+        if check == "push":
+            return "sole"
+    
+    @staticmethod
+    def clutch(pares):
+        if pares == 'par':
+            return "clutch"
+        warnings.warn("No")
+        
+    @abstractmethod
+    def steering(check):
+        pass
+
+    @abstractmethod
+    def horn(check):
+        return True
+    
+    @abstractmethod
+    def brake_light(check):
+        return True
+    
+    def front_light(check):
+        return
+    
+    @abstractmethod
+    def gears(self, total_gears):
+        return f"Gears {total_gears}"
+    
+
+    @abstractmethod
+    def ignition_switch(self, switch):
+        if switch == 'on':
+            return 'ON'
+        return "OFF"
+    
+    @abstractmethod
+    def brack(self, check):
+        if check == "pras":
+            return "sole"
 
 class DrivableonRoad(ABC):
     pass
